2960e973c523d622c461931f6e0c4b7509d6f8d1
s174426 <s174426@student.dtu.dk>
2021-04-27 10:22:47 +0200

    Removed most priority stuff

--- main/src/Object.java
+++ main/src/Object.java
@@ -4,22 +4,14 @@ import java.util.Set;
 
 import java.util.Objects;
 
-public abstract class Object implements Comparable<Object> {
-    int priority;
-    int originalPriority;
+public abstract class Object{
+
     char ID;
     Node position;
     Node Goal;
     Plan mainPlan = new Plan();
 
 
-
-    public char getID(){
-        return ID;
-    }
-    public void setID(char ID) {
-        this.ID = ID;
-    }
     public void setGoal(Node goal) {
         this.Goal = goal;
     }
@@ -27,29 +19,14 @@ public abstract class Object implements Comparable<Object> {
     abstract boolean isInGoal();
 
 
-
-    public void setPriority(){
-        this.priority = ID - '0';
-        this.originalPriority = ID - '0';
-    }
-
-
     public abstract void planPi(Map map, LinkedHashSet visited);
 
     abstract void bringBlank(State state, Map map, Agent otherAgent);
 
 
-
-
-    // Allows comparison and sorting w.r.t priority
-    @Override
-    public int compareTo(Object o) {
-        return Integer.compare(priority, o.priority);
-    }
-
     @Override
     public String toString(){
-        return "ID: " + ID + ", priority: " + priority + ", position: " + position.toString();
+        return "ID: " + ID + ", position: " + position.toString();
 
     }
 
